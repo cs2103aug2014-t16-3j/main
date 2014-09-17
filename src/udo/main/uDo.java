101a9acc05bbc52a38b592c6a5f83e89857c7a72
Nicholas Lum <nicholaslum444@hotmail.com>
2014-09-17 15:57:06 +0800

    put the general flow into the uDo class
    this will be the guideline for our APIs.
    
    Signed-off-by: Nicholas Lum <nicholaslum444@hotmail.com>

--- src/udo/main/uDo.java
+++ src/udo/main/uDo.java
@@ -1,9 +1,29 @@
 package udo.main;
 
 public class uDo {
-
+	
 	public static void main(String[] args) {
+		uDo udo = new uDo();
+		udo.run(args);
+	}
+
+	void run(String[] args) {
 		
+		String filename = getFilename(args);
+		engine.loadFile(filename);
+		
+		while (isRunning) {
+			String inputString = ui.getInput();
+			String outputString = udo.parseAndExecute(input);
+			ui.show(outputString);
+		}
+	}
+	
+	String parseAndExecute(String input) {
+		Command cmd = parser.getCmd(input);
+		InputData inputData = parser.getData(cmd, input);
+		OutputData outputData = engine.execute(cmd, inputData);
+		String output = feedback.format(outputData);
 	}
 
 }
