0b1ca1b889dbcb16f754506543d31153ffc819d4
Nicholas Lum <nicholaslum444@hotmail.com>
2014-09-21 16:17:36 +0800

    added some stuff to recycle bin
    Signed-off-by: Nicholas Lum <nicholaslum444@hotmail.com>

--- src/udo/main/RecycleBin.java
+++ src/udo/main/RecycleBin.java
@@ -1,5 +1,32 @@
 package udo.main;
 
-public class RecycleBin {
+import udo.util.Command;
+import udo.util.ItemData;
 
+public class RecycleBin {
+	ItemData mItem;
+	Command mCommand;
+	
+	public RecycleBin() {
+		mItem = null;
+		mCommand = null;
+	}
+	
+	public ItemData putItem(ItemData item) {
+		mItem = item;
+		return mItem;
+	}
+	
+	public Command putCommand(Command cmd) {
+		mCommand = cmd;
+		return mCommand;
+	}
+	
+	public ItemData getItem() {
+		return mItem;
+	}
+	
+	public Command getCommand() {
+		return mCommand;
+	}
 }
