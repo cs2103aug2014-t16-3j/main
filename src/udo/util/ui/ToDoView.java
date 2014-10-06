35559c9559275c99266732aba0e4070bac246055
sswander <leonardosyahputra@yahoo.com>
2014-10-06 16:49:14 +0800

    todo view started

--- src/udo/util/ui/ToDoView.java
+++ src/udo/util/ui/ToDoView.java
@@ -1,5 +1,28 @@
 package udo.util.ui;
 
-public class ToDoView {
+import java.awt.Dimension;
 
+import javax.swing.JPanel;
+
+public class ToDoView extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	private final static int VIEW_HEIGHT = 550;
+	private final static int VIEW_WIDTH = 360;
+	
+	public ToDoView() {
+		setPreferredSize(new Dimension(VIEW_WIDTH,VIEW_HEIGHT));
+		setBounds(20,20,VIEW_WIDTH,VIEW_HEIGHT);
+		setOpaque(false);
+	}
+	
+	public void init() {
+		initHeader();
+		populateView();
+	}
+	
+	public void initHeader() {
+		
+	}
 }
