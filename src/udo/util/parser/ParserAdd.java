6b9f6a1973e81413bd6b7afcda845e05656b4985
jiawei <jaewei.j.w@gmail.com>
2014-10-12 20:10:00 +0800

    Differentiating between event, task and plans
    - clarify when does parsing fail

--- src/udo/util/parser/ParserAdd.java
+++ src/udo/util/parser/ParserAdd.java
@@ -1,5 +1,8 @@
 package udo.util.parser;
 
+import udo.util.shared.Command;
+import udo.util.shared.InputData;
+
 /** 
  * This class takes in ADD commands and break them into the following:
  * ADD_EVENT, ADD_TASK, ADD_PLAN
@@ -14,7 +17,51 @@ package udo.util.parser;
 public class ParserAdd {
 
 	public ParserAdd() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	
+	/**
+	 * This method takes in user input and returns an InputData of the following 
+	 * CommandType: ADD_EVENT, ADD_TASK, ADD_PLAN
+	 * and the stores them in the necessary fields.
+	 * 
+	 * @param type
+	 * @param details
+	 * @return an InputData
+	 */
+	public InputData add(Command type, String details) {
+		InputData add = null;
+		if (isEvent(details)) {
+			add = new InputData(Command.ADD_EVENT);
+			// tags
+		} else if (isTask(details)) {
+			add = new InputData(Command.ADD_TASK);
+			// tags
+			// check if valid task
+		} else if (isPlan(details)) {
+			add = new InputData(Command.ADD_PLAN);
+			// tags
+			// check if valid plan
+		} else {
+			// Parsing Status failed
+		}
+		return add;
 	}
 
+	// checks if there is 2 date and 2 time
+	// 
+	private boolean isEvent(String details) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean isTask(String details) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean isPlan(String details) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
