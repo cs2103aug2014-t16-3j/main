c58ee4e809639813dc820d4b7b2f645e9ce6176c
jiawei <jaewei.j.w@gmail.com>
2014-10-15 13:33:03 +0800

    Created all edit fields methods
    - yet to test them

--- src/udo/util/parser/ParserEdit.java
+++ src/udo/util/parser/ParserEdit.java
@@ -12,7 +12,7 @@ import udo.util.shared.InputData;
 
 public class ParserEdit {
 	
-	private int startingIndex = 6;
+	// private int startingIndex = 6;
 	// assume edit details starts after edit command, e.g. "edit <fields>"
 
 	public ParserEdit() {
@@ -21,43 +21,121 @@ public class ParserEdit {
 	//edit <uid> <field> <new-info>
 	public InputData edit(Command type, String details) {
 		InputData data = new InputData(type);
-		
+		// check for null
+		// put tag
 		return data;
 	}
 	
+	// returns uid if it exists
+	// otherwise returns -1
 	public int getUid(String details) {
-		return 0;
+		String[] words = details.split(" ");
+		String uidString = words[1];
+		int uid = -1;
+		try {
+			 uid = Integer.parseInt(uidString);
+		} catch (NumberFormatException nfe) {
+			
+		}
+		return uid;
 	}
 	
+	// returns one of the fields if it exists
+	// otherwise returns null
 	public String getField(String details) {
+		String[] fields = {"title",
+				"start time", "end time", 
+				"start date", "end date",
+				"due time", "due date"};
+		for (int i = 0; i < fields.length; i++) {
+			if (details.contains(fields[i])) {
+				return fields[i];
+			}
+		}
 		return null;
 	}
 	
+	// returns the new title if it exists
+	// otherwise returns null
 	public String getTitle(String details) {
-		return null;
+		int startingIndex = 11; // new info starts after "edit title "
+		try {
+			String title = details.substring(startingIndex);
+			return title;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getStartTime(String details) {
-		return null;
+		int startingIndex = 16; // new info starts after "edit start time "
+		try {
+			String timeString = details.substring(startingIndex);
+			ParserTime startTime = new ParserTime();
+			Calendar cal = startTime.getTime(timeString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getEndTime(String details) {
-		return null;
+		int startingIndex = 14; // new info starts after "edit end time "
+		try {
+			String timeString = details.substring(startingIndex);
+			ParserTime endTime = new ParserTime();
+			Calendar cal = endTime.getTime(timeString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getStartDate(String details) {
-		return null;
+		int startingIndex = 16; // new info starts after "edit start date "
+		try {
+			String dateString = details.substring(startingIndex);
+			ParserDate startDate = new ParserDate();
+			Calendar cal = startDate.getDate(dateString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getEndDate(String details) {
-		return null;
+		int startingIndex = 14; // new info starts after "edit end date "
+		try {
+			String dateString = details.substring(startingIndex);
+			ParserDate endDate = new ParserDate();
+			Calendar cal = endDate.getDate(dateString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getDueTime(String details) {
-		return null;
+		int startingIndex = 14; // new info starts after "edit due time "
+		try {
+			String timeString = details.substring(startingIndex);
+			ParserTime dueTime = new ParserTime();
+			Calendar cal = dueTime.getTime(timeString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 	
 	public Calendar getDueDate(String details) {
-		return null;
+		int startingIndex = 14; // new info starts after "edit due date "
+		try {
+			String dateString = details.substring(startingIndex);
+			ParserDate dueDate = new ParserDate();
+			Calendar cal = dueDate.getDate(dateString);
+			return cal;
+		} catch (IndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 }
